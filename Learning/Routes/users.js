5df61d83d6e35327bf8d5b6a5e0329a4f97ece9d
Vinicius Euleoterio <vinicius.euleoterio@hotmail.com>
2019-06-11 20:37:39 -0300

    Add model, database

--- Learning/Routes/users.js
+++ Learning/Routes/users.js
@@ -1,16 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../model/user');
 
 router.get('/', (req, res) => {
+    User.find({}, (err,data) => {
+        if (err) return res.send({ error: 'Erro na consulta de usuários' });
+        return res.send(data);
+    })
+
     return res.send({message: 'Tudo ok com o método GET dos usuários'});
 });
 
-router.post('/', (req, res) => {
-    return res.send({message: 'Tudo ok com o método POST dos usuários'});
-});
+//no root não estou usando 
+// router.post('/', (req, res) => {
+//     return res.send({message: 'Tudo ok com o método POST dos usuários'});
+// });
 
 router.post('/create', (req, res) => {
-    return res.send({message: 'Seu usuário foi criado.'});
+    const { email, password } = req.body;
+
+    if(!email || !password) return res.send( {error: 'Dados insuficientes'});
+
+    User.findOne({email}, (err,data) => {
+        if (err) return res.send({ erro:'Erro ao buscar usuário!' });
+        if (date) return res.send({ erro: 'Usuário já registrado.'});
+    });
+
+    User.create(req.body, (err, data) => {
+        if (err) return res.send({ erro: 'Erro ao criar usuário.' });
+        return res.send(data);
+    });
 });
 
 module.exports = router;
